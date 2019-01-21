525170edeb9de9d76506b7c79b42c839eaa419eb
unknown <dragostinj@gmail.com>
2019-01-21 10:40:12 +0200

    Updates

---
+++ precache-manifest.4fb4e45578e50fbb4a152d709a1a113e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/stargame/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "829c9727f2758dab422a",
+    "url": "/stargame/static/css/main.c13ac6f8.chunk.css"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/stargame/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e4365d0f55f697bbe8d6",
+    "url": "/stargame/static/js/1.e4365d0f.chunk.js"
+  },
+  {
+    "revision": "31fca53619d968056b83",
+    "url": "/stargame/static/js/runtime~main.31fca536.js"
+  },
+  {
+    "revision": "829c9727f2758dab422a",
+    "url": "/stargame/static/js/main.829c9727.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/stargame/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/stargame/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/stargame/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e4365d0f55f697bbe8d6",
+    "url": "/stargame/static/css/1.23b72801.chunk.css"
+  },
+  {
+    "revision": "f8ce6ed1e9a889dfeab2f71ebc2de34e",
+    "url": "/stargame/index.html"
+  }
+];
